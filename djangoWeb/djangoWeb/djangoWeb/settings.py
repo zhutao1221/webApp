26574875a9a89de52e410bbfeb5c70b03c47414a
zhutao <zhutao1221@126.com>
2017-12-01 17:25:25 +0800

    web

--- djangoWeb/djangoWeb/djangoWeb/settings.py
+++ djangoWeb/djangoWeb/djangoWeb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls',
+    #'MachineLearningService',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'djangoWeb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,7 +68,7 @@ TEMPLATES = [
         },
     },
 ]
-
+print(os.path.join(BASE_DIR, 'templates'))
 WSGI_APPLICATION = 'djangoWeb.wsgi.application'
 
 
--- djangoWeb/djangoWeb/polls/views.py
+++ djangoWeb/djangoWeb/polls/views.py
@@ -1,16 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from .models import Choice, Question
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+def hello(request):
+    context          = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'hello.html', context)
