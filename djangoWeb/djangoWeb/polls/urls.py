57806f329b254467e0ea6334fa25d8d7de0e9062
zhutao <zhutao1221@126.com>
2017-12-05 10:44:24 +0800

    web

--- djangoWeb/djangoWeb/polls/urls.py
+++ djangoWeb/djangoWeb/polls/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     url(r'^hello$', views.hello, name='hello'),
-    #url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     # ex: /polls/5/
-    #url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
-    #url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
